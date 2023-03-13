ca09db7fe14198500a080efd63a7f5356dac857f
otmanTR <r.brk.otman@gmail.com>
2023-03-13 14:24:07 +0300

    add Missions.jsx page

---
+++ src/page/Missions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Mission from '../components/Misson';
+
+export default function Missions() {
+  return (
+    <>
+      <div>
+        <Mission />
+      </div>
+    </>
+  );
+}
