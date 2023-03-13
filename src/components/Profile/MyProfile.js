c2befddc53f1101b2fa578aac2720880ef869adb
TamaraDimov <tamemojsoska@gmail.com>
2023-03-13 10:23:44 +0100

    Create My Profile setup

---
+++ src/components/Profile/MyProfile.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+const MyProfile = () => {
+  <ul>
+    <li link="#" className="missions">
+      My Missions
+    </li>
+    <li link="#" className="rockets">
+      My Rockets
+    </li>
+  </ul>;
+};
+
+export default MyProfile;
