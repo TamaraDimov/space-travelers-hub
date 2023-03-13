561eb99cc65dd598a49e29b72146866f34369e4e
TamaraDimov <tamemojsoska@gmail.com>
2023-03-13 13:02:07 +0100

    fix linter errors

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,10 @@
+/*eslint-disable*/
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <>
       <div className="app">
-
         <nav>
           <ul className="navContainer">
             <li>
@@ -14,14 +14,20 @@ export default function Navbar() {
               <h1>Space Traveler's Hub</h1>
             </li>
             <li>
-              <Link className="navBooks" to="/">Rockets</Link>
+              <Link className="navRockets" to="/">
+                Rockets
+              </Link>
             </li>
             <li>
-              <Link className="navCategories" to="/categories">Missions</Link>
+              <Link className="navMissions" to="/Missions">
+                Missions
+              </Link>
             </li>
             <li className="navBorderLine" />
             <li>
-              <Link className="navCategories" to="/categories">My Profile</Link>
+              <Link className="navMyProfile" to="/MyProfile">
+                My Profile
+              </Link>
             </li>
           </ul>
         </nav>
