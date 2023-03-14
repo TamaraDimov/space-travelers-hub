97f473eb52e3efe15098eaa2c936ae1d1bbc4d9b
otmanTR <r.brk.otman@gmail.com>
2023-03-14 18:19:07 +0300

    add display function

--- src/components/Misson.js
+++ src/components/Misson.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Mission({ mission, description, status }) {
+export default function Mission({ name, description, status }) {
   return (
     <>
       <section className="missions">
@@ -15,7 +15,7 @@ export default function Mission({ mission, description, status }) {
         </div>
         <div>
           <ul className="missionsIndex">
-            <li><h2>{mission}</h2></li>
+            <li><h2>{name}</h2></li>
             <li>
               <p>
                 {description}
@@ -32,7 +32,7 @@ export default function Mission({ mission, description, status }) {
 }
 
 Mission.propTypes = {
-  mission: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   status: PropTypes.string.isRequired,
 };
--- src/page/Missions.jsx
+++ src/page/Missions.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMissons } from '../redux/missions/missionSlice';
 import Mission from '../components/Misson';
 
 export default function Missions() {
+  const { missionList, status } = useSelector((store) => store.missions);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(getMissons());
+    }
+    if (status === 'succeed') {
+      dispatch(getMissons());
+    }
+  }, [status, dispatch]);
+
   return (
     <>
       <div>
-        <Mission />
+        { missionList.map((mission) => (
+          <Mission
+            key={mission.id}
+            id={mission.id}
+            name={mission.name}
+            description={mission.description}
+          />
+        ))}
+
       </div>
     </>
   );
