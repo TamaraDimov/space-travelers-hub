420f00fa9947d72e1ed9a16cc6e13f818af5b7f6
otmanTR <r.brk.otman@gmail.com>
2023-03-16 12:21:32 +0300

    linters fix

--- src/components/ProfileMissions.jsx
+++ src/components/ProfileMissions.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-// import { allMissions } from '../redux/missions/missionSlice';
 
 export default function ProfileMissions() {
   const { missionList } = useSelector((store) => store.missions);
-  // console.log('we are here', missionList);
   const joinedMission = missionList.filter((mission) => mission.reserved === true);
   if (joinedMission.length === 0) {
     return (
