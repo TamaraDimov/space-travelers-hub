e3462380fb12f55bb6841af31ba8b42db770dbb8
TamaraDimov <tamemojsoska@gmail.com>
2023-03-17 10:15:29 +0100

    make use of React conditional rendering syntax

--- src/components/Mission/ProfileMissions.js
+++ src/components/Mission/ProfileMissions.js
@@ -6,31 +6,20 @@ export default function ProfileMissions() {
   const joinedMission = missionList.filter(
     (mission) => mission.reserved === true,
   );
-  if (joinedMission.length === 0) {
-    return (
-      <>
-        <div>
-          <div>
-            <h2 className="reservationTableTitle"> My Missions</h2>
-
-            <p className="reservationTable">No joined missions</p>
-          </div>
-        </div>
-      </>
-    );
-  }
   return (
-    <>
-      <div>
-        <h2 className="reservationTableTitle"> My Missions</h2>
-      </div>
-      <div>
-        {joinedMission.map((mission) => (
-          <div key={mission.id}>
-            <p className="reservationTable">{mission.name}</p>
-          </div>
-        ))}
-      </div>
-    </>
+    <div>
+      <h2 className="reservationTableTitle"> My Missions</h2>
+      {joinedMission.length === 0 ? (
+        <p className="reservationTable">No joined missions</p>
+      ) : (
+        <div>
+          {joinedMission.map((mission) => (
+            <div key={mission.id}>
+              <p className="reservationTable">{mission.name}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
--- src/components/Rocket/ProfileRockets.js
+++ src/components/Rocket/ProfileRockets.js
@@ -4,30 +4,21 @@ import { useSelector } from 'react-redux';
 export default function ProfileRockets() {
   const { rocketList } = useSelector((store) => store.rockets);
   const joinedRockets = rocketList.filter((rocket) => rocket.reserved === true);
-  if (joinedRockets.length === 0) {
-    return (
-      <>
-        <div>
-          <div>
-            <h2 className="reservationTableTitle"> My Rockets</h2>
-            <p className="reservationTable">No booked rockets</p>
-          </div>
-        </div>
-      </>
-    );
-  }
+
   return (
-    <>
-      <div>
-        <h2 className="reservationTableTitle"> My Rockets</h2>
-      </div>
-      <div>
-        {joinedRockets.map((rocket) => (
-          <div key={rocket.id}>
-            <p className="reservationTable">{rocket.name}</p>
-          </div>
-        ))}
-      </div>
-    </>
+    <div>
+      <h2 className="reservationTableTitle"> My Rockets</h2>
+      {joinedRockets.length === 0 ? (
+        <p className="reservationTable">No booked rockets</p>
+      ) : (
+        <div>
+          {joinedRockets.map((rocket) => (
+            <div key={rocket.id}>
+              <p className="reservationTable">{rocket.name}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
