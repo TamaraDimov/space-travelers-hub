b15a90fe3505f569e5dca0ba03ac40e986070a9f
TamaraDimov <tamemojsoska@gmail.com>
2023-03-17 08:13:59 +0100

    add test for missions

--- src/components/test/ProfileMissions.js
+++ src/components/test/ProfileMissions.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
+import ProfileMissions from '../Mission/ProfileMissions';
 import Mission from '../../page/Missions';
 
 test('myProfile Page renders correctly', () => {
